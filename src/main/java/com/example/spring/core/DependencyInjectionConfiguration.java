c0e4cb3c48addc425367afc981ae4b0eb7fc3c46
Maurice Marvin <maurice.marvin8812@gmail.com>
2022-03-01 00:03:48 +0700

    Memilih dependency ketika dependency injection

--- src/main/java/com/example/spring/core/DependencyInjectionConfiguration.java
+++ src/main/java/com/example/spring/core/DependencyInjectionConfiguration.java
@@ -3,21 +3,30 @@ package com.example.spring.core;
 import com.example.spring.core.data.Bar;
 import com.example.spring.core.data.Foo;
 import com.example.spring.core.data.FooBar;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 public class DependencyInjectionConfiguration {
+
+    @Primary
     @Bean
-    public Foo foo(){
+    public Foo fooFirst() {
         return new Foo();
     }
 
     @Bean
-    public Bar bar(){
+    public Foo fooSecond() {
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
         return new Bar();
     }
 
     @Bean
-    public FooBar fooBar(Foo foo, Bar bar){
+    public FooBar fooBar(@Qualifier("fooSecond") Foo foo, Bar bar) {
         return new FooBar(foo, bar);
     }
 }
